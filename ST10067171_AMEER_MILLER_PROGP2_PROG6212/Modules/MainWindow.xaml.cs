d5145b0093b3be0907fe017fd7b6fcebe94f067e
MillerAmeer <102788674+MillerAmeer@users.noreply.github.com>
2023-11-06 07:15:55 +0000

    Add files via upload

---
+++ ST10067171_AMEER_MILLER_PROGP2_PROG6212/Modules/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Modules
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            Register reg = new Register();
+            reg.Show(); 
+            this.Close();
+        }
+
+        private void login_Click(object sender, RoutedEventArgs e)
+        {
+            string UserName = User_Name.Text;
+            string PassWord = Pass_wrd.Text;
+        }
+        private void OnExit(object sender, ExitEventArgs e)
+        {
+            Properties.Settings.Default.Save();
+        }
+    }
+}
---
+++ ST10067171_AMEER_MILLER_PROGP2_PROG6212/Modules/Register.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Modules
+{
+    /// <summary>
+    /// Interaction logic for Register.xaml
+    /// </summary>
+    public partial class Register : Window
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            string connectionstring = "";
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionstring))
+                {
+                    string insertQuesrt = "INSERT INTO STUDENT(UserName)VALUES(@STUDENT_USERNAME)";
+                    SqlCommand command = new SqlCommand(insertQuesrt, con);
+
+                    command.Parameters.AddWithValue("@STUDENT_USERNAME", UserName.Text);
+                    command.ExecuteNonQuery();
+                    MessageBox.Show(UserName.Text + "added successfully");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ERROR:" + ex.Message);
+            }
+            UserName.Text = "";
+            Password.Text = "";
+        }
+    }
+}
+
+
+
